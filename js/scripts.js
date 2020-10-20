e0972c815695ef09ea66b5337ae890d5123c49b7
Amanda Otero <mandystar018@gmail.com>
2020-10-20 13:58:42 -0700

    Add class, and jQuery library, link on the tag for boostrap and styles
    Co-authored-by: Jerrod Styrk <jstyrk@citadel.edu>

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $(".solid").click(function() {
+    $(".solid").addClass("border1");
+  });
+  $(".dotted").click(function(){
+    $(".dotted").addClass("border2")
+  }); 
+});
